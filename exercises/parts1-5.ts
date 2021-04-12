5ebdddfac01c747b1a8eb863c2f89d318832c59b
Dulfite <61243073+Dulfite@users.noreply.github.com>
2021-04-12 13:50:09 -0500

    Completed most of exercise.

--- exercises/parts1-5.ts
+++ exercises/parts1-5.ts
@@ -3,29 +3,58 @@
 
 
 // Part 1: Declare (5) Variables With Type
-
+// let spacecraftName: string = "Determination";
+// let speedMph: number = 17500;
+let kilometerToMars: number = 225000000;
+let kilometersToTheMoon: number = 384400;
+// let milesPerKilometer: number = 0.621;
 
 
 // Part 2: Print Days to Mars
-
+let milesToMars: number = kilometerToMars*speedMph;
+let hoursToMars: number = milesToMars/speedMph;
+let daysToMars: number = hoursToMars/24;
 
 
 // Code an output statement here (use a template literal):
 
 
-
 // Part 3: Create a Function ("getDaysToLocation")
-
+function getDaysToLocation(kilometersAway: number): number {
+    let milesAway: number = kilometersAway * milesPerKilometer;
+    let hours: number = milesAway / speedMph;
+    return hours / 24;
+}
 
 
 // Move your output statement from part 2 here. Update the template literal to call
 // the function and print the outputs for a Mars trip and a moon trip.
+console.log(`${spacecraftName} would take ${daysToMars} days to get to Mars and .`)
 
 
 
 
 // Part 4: Create a Spacecraft Class
-
+class Spacecraft {
+    milesPerKilometer: number = 0.621;
+    name: string;
+    speedMph: number;
+ 
+    constructor(name: string, speedMph: number) {
+       this.name = name;
+       this.speedMph = speedMph;
+    }
+ 
+    getDaysToLocation(kilometersAway: number): number {
+       let milesAway: number = kilometersAway * this.milesPerKilometer;
+       let hours: number = milesAway / this.speedMph;
+       return hours / 24;
+    }
+ 
+    printDaysToLocation(location: SpaceLocation) {
+       console.log(`${this.name} would take ${this.getDaysToLocation(location.kilometersAway)} days to get to ${location.name}.`);
+    }
+ }
 
 
 
